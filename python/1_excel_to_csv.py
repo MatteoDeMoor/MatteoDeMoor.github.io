0b3aea361b6780a7e32071ed7f3e64aeb973de03
MatteoDeMoor <125797081+MatteoDeMoor@users.noreply.github.com>
2025-02-15 13:54:36 +0100

    Update script to change excel to csv

--- python/1_excel_to_csv.py
+++ python/1_excel_to_csv.py
@@ -14,7 +14,7 @@ output_dir = r'D:\Hogent\Visual Studio Code\Projecten\SiteGithub\MatteoDeMoor.gi
 os.makedirs(output_dir, exist_ok=True)
 
 # Stel het volledige pad voor de CSV in
-output_path = os.path.join(output_dir, 'shirts_from_excel.csv')
+output_path = os.path.join(output_dir, 'Shirts.csv')
 
 # Converteer de data naar een CSV-bestand en sla het op
 df.to_csv(output_path, index=False)
