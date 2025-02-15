62f795041e4dec3a68fd57bc8c7dc17a1e19e1d3
MatteoDeMoor <125797081+MatteoDeMoor@users.noreply.github.com>
2025-02-15 01:09:55 +0100

    Update python script, excel to csv

--- python/1_excel_to_csv.py
+++ python/1_excel_to_csv.py
@@ -2,10 +2,15 @@ import pandas as pd
 import os
 
 # Lees het Excel-bestand in
-df = pd.read_excel(r'D:\Hogent\Visual Studio Code\Projecten\SiteGithub\MatteoDeMoor.github.io\excel\Shirts.xlsx')
+df = pd.read_excel(
+    r'D:\Hogent\Visual Studio Code\Projecten\SiteGithub\MatteoDeMoor.github.io\excel\Club_Shirts.xlsx'
+)
+
+# Filter de DataFrame zodat alleen rijen met een numerieke 'ID' behouden blijven
+df = df[pd.to_numeric(df['ID'], errors='coerce').notna()]
 
 # Bepaal de output directory en maak deze aan als deze nog niet bestaat
-output_dir = r'D:\Hent\Visual Studio Code\Projecten\SiteGithub\MatteoDeMoor.github.io\csv'
+output_dir = r'D:\Hogent\Visual Studio Code\Projecten\SiteGithub\MatteoDeMoor.github.io\csv'
 os.makedirs(output_dir, exist_ok=True)
 
 # Stel het volledige pad voor de CSV in
